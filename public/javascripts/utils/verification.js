47cbfb8be1d4afcac154b4378ea30edd0b5f2ee2
Sidney  Tróia <sidneytroia@Air-de-Sidney.home>
2023-03-03 22:49:32 +0000

    base

--- public/javascripts/utils/verification.js
+++ public/javascripts/utils/verification.js
@@ -1,8 +1,8 @@
 create table Livro (
     Livro_id SERIAL not null,
     Titulo  VARCHAR(20) not null,							--4 algarismos, com 2 casas decimais
-    Livro__id int ,
-    Livro_Lançamento date not null,
+    Livro_Autor_id int ,
+    Livro_Lançamento Int,
     Livro_Volume int,
     Livro_oferta_id int,
     Livro_contraP int,
@@ -11,19 +11,20 @@ create table Livro (
 create table Autor (
     Autor_id SERIAL not null,
     Autor_nome VARCHAR(20) not null,							--4 algarismos, com 2 casas decimais
-    primary key (Autor_id));
+    primary key (Autor_id),
+);
 create table Oferta (
     Oferta_id SERIAL not null,
     Oferta_nome VARCHAR(20) not null,
     Oferta_Trans_Id int,
-    Oferta_Dia date not null,
+    Oferta_Dia Datetime,
     Oferta_contaP int,
     primary key (Oferta_id)
 );
 create table usuario (
     user_id_ SERIAL not null,
     user_nome VARCHAR not null,
-    user_hbd date not null, 								
+    user_hbd DateTime, 								
     user_email varchar NOT null UNIQUE,
     user_oferta_id int ,
     user_transacao_id int ,							
@@ -52,7 +53,8 @@ create table TL(
 TM_id serial not null,
 TL_Trasancao_id int not null,
 TL_Local_id int not null,
- primary key (TM_id));
+ primary key (TM_id),
+);
 
 create table ET(
 ET_id serial not null,
@@ -64,13 +66,12 @@ primary key (ET_id)
 create table Troca(
 Troca_id serial not null,
 TL_Trasancao_id int not null,
-TL_Livro_id int not null,
-TL_oferta_id int not null,
+TL_Local_id int not null,
 primary key (Troca_id) 
 );
 create table AL(
     Al_id serial not null,
-    Al_Autor_id int not null,
+    AL_Autor_id int not null,
     Al_livro_id int not null,
-    primary key (Al_id)
+    primary key (Troca_id)
     );
